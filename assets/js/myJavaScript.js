0c456faa30addbafde406745aeac036d9f0819e7
bengray <bendoylegray@gmail.com>
2017-08-20 13:23:13 -0400

    Completing mobile-friendly view

--- assets/js/myJavaScript.js
+++ assets/js/myJavaScript.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
     $("#hamburger-button").click(function() {
         $("#mobile-nav").toggle("fast");
 
-
+        $("#close-button").toggle();
+        $("#hamburger-button").toggle();
     });
 
     $("#close-button").click(function() {
